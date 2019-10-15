bc1c2903de5b5c5b4307ce964c8592eaae53f0d3
adrianTJenkins <adrian.jenkins99@gmail.com>
2019-10-15 01:13:33 -0400

    Implemented tanh
    Class is now fit for reuse

--- nn.py
+++ nn.py
@@ -2,32 +2,31 @@ import numpy as np
 
 class NN:
 
-    def __init__(self, name):
+    def __init__(self, name, training_in, training_out):
+        np.random.seed(1)
         self.activation_name = name
-
-    activation_name = ""
-    training_inputs = np.array([[0,0,1],
-                                [1,1,1],
-                                [1,0,1],
-                                [0,1,1]])
-
-    training_outputs = np.array([[0,1,1,0]]).T
-
-    np.random.seed(1)
-
-    synaptic_weights = 2 * np.random.random((3, 1)) - 1
+        self.training_inputs = training_in
+        self.training_outputs = training_out
+        self.synaptic_weights = 2 * np.random.random((3, 1)) - 1
+    
     def sigmoid(self, x, is_derivative = False):
         if is_derivative:
             return x * (1 - x)
-        return 1 / (1 + np.exp(-x))
+        return 1 / (1 + np.e ** (-x))
 
-    print('Random starting synaptic weights: ')
-    print(synaptic_weights)
+    def tanH(self, x, is_derivative = False):
+        if is_derivative:
+            return 1 - np.tanh(x) ** 2
+        else:
+            return np.tanh(x)
+
+    
         
 
     def learn(self, activation_method, x_value = 0, bool_value = False):
+        print('Random starting synaptic weights: ')
+        print(self.synaptic_weights)
         for i in range(100000):
-
             input_layer = self.training_inputs
 
             x_value = np.dot(input_layer, self.synaptic_weights)
@@ -42,18 +41,32 @@ class NN:
 
             self.synaptic_weights += np.dot(input_layer.T, adjustments)
 
-        print(self.activation_name)
-        print('Synaptic weights after training')
+        print("\nActivation Function: " + self.activation_name)
+        print('\nSynaptic weights after training\n')
         print(self.synaptic_weights)
 
         print('\nOutputs after training')
         print(outputs)
+        print('')
+        print('')
 
     def main(self):
         if self.activation_name == "sigmoid":
             self.learn(self.sigmoid)
+        elif self.activation_name == "tanH":
+            self.learn(self.tanH)
         else:
             print(self.activation_name)
 
-n = NN("sigmoid")
-n.main()
+t_in = np.array([[0,0,1],
+                 [1,1,1],
+                 [1,0,1],
+                 [0,1,1]])
+
+t_out = np.array([[0,1,1,0]]).T
+
+sig_net = NN("sigmoid", t_in, t_out)
+sig_net.main()
+
+tan_net = NN("tanH", t_in, t_out)
+tan_net.main()
